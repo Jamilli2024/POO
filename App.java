f99d0c0a27e825b911efae7798849af7ef14536d
Jamilli2024 <jamilli.silva2013@gmail.com>
2025-02-18 22:51:25 +0000

    aula3

---
+++ App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class App {
+    public static boolean ehPalindromo(String input){
+            return false;
+
+    }
+
+
+    public static void main(String[] args) {
+        String palavra;
+                Scanner ler = new Scanner(System.in);
+        
+                System.out.println("Digite uma frase:");
+                 palavra =ler.next();
+    
+        
+            }
+        
+        
+            private static void palavra() {
+                throw new UnsupportedOperationException("Unimplemented method 'palavra'");
+            }
+}
