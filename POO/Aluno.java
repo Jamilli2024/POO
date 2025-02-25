900f36490545438a4b136743d3b9b20ef4dfa959
Jamilli2024 <jamilli.silva2013@gmail.com>
2025-02-25 20:45:06 +0000

    atualizando

--- POO/Aluno.java
+++ POO/Aluno.java
@@ -3,12 +3,15 @@ package POO;
 public class Aluno {
     public String nome;
     public double altura;
-
-    public Aluno(){
-
+ 
+    public Aluno() {
     }
-
-    public void estudar(String var1){
-        System.out.println("O aluno solicitou a nota:");
+ 
+    public void estudar(String var1) {
+       System.out.println(this.nome + " estuda " + var1);
     }
-}
+ 
+    public void pedirNota(String var2) {
+       System.out.println(this.nome + " está pedindo a nota ao professor " + var2);
+    }
+ }
--- POO/App.java
+++ POO/App.java
@@ -1,5 +1,34 @@
 package POO;
 
 public class App {
-    
+
+
+    public static void main(String[] args) throws Exception {
+        Aluno aluno1 = new Aluno();
+        Aluno aluno2 = new Aluno();
+      
+        aluno1.nome = "Arthur";
+        aluno1.altura = 1.45;
+
+        aluno2.nome = "Jamilli Silva";
+        aluno2.altura = 1.63;
+        
+        System.out.println("nome do aluno:   "   +  aluno1.nome);       
+        System.out.println("Altura: " + aluno1.altura);
+
+        aluno1.nome = "Jamilli";
+
+         System.out.println("Aluno atualizado é:  " +  aluno1.nome);
+
+
+         System.out.println("nome do aluno:   "   +  aluno2.nome);       
+         System.out.println("Altura: " + aluno2.altura);
+
+         aluno1.estudar("POO");
+        
+         aluno1.pedirNota("Iago" );
+
+        
+
+    }
 }
